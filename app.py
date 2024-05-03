7e6fac1b9b3dc9622da7ff9ba46fb1e712ebbdee
zadiak247 <145591529+zadiak247@users.noreply.github.com>
2024-05-03 12:42:44 +0000

    Initial commit

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return render_template("index.html", title="Hello")
+def main():
+    if 0>1:
+        return render_template("index.html", title="")
+    return redirect('/news')
+
+
+@app.route("/news")
+def news():
+    return render_template("news.html", title="news")
+
+
+@app.route("/feedback")
+def deedback():
+    return render_template("feedback.html", title="feedback")
